020ee314710b94343fb478e1f9f45b0abaf5c7f9
Alphilus <58624681+Alphilus@users.noreply.github.com>
2024-03-27 16:31:40 +0700

    Update main.js

--- Day4/main.js
+++ Day4/main.js
@@ -10,25 +10,34 @@ let points = 0;
 let count = document.getElementById('score');
 let pointsElement = count.querySelector('.points');
 
-document.getElementById('btn').addEventListener('click', function(){
-    for (let i = 0; i < colors.length; i++) {
-        let box = document.createElement('div');
-        box.className = 'box';  
+for (let i = 0; i < colors.length; i++) {
+    let box = document.createElement('div');
+    box.className = 'box';
+        
+    let fiveColor = colors[i];
+    box.style.backgroundColor = fiveColor;
 
+    box.addEventListener('click', function(){
+        box.parentNode.removeChild(box);
+        points--;
+        pointsUpdates();
+    });
+
+    document.querySelector(".boxes").appendChild(box);
+    points++;
+
+    document.getElementById('btn').addEventListener('click', function(){
+        let box = document.createElement('div');
+        box.className = 'box';
+        
         let fiveColor = colors[i];
         box.style.backgroundColor = fiveColor;
-
-        box.addEventListener('click', function(){
-            box.parentNode.removeChild(box);
-            points--;
-            pointsUpdates();
-        });
-
         document.querySelector(".boxes").appendChild(box);
         points++;
-    }
-    pointsUpdates();
-})
+        pointsUpdates();
+    });
+}
+pointsUpdates();
 
 function pointsUpdates(){
     pointsElement.innerHTML = points;
