c6439aa79b75a515cf8311f399160ede337abb9f
Alphilus <58624681+Alphilus@users.noreply.github.com>
2024-03-27 16:23:09 +0700

    Add files via upload

---
+++ Day4/main.js
@@ -0,0 +1,35 @@
+let colors = [
+    '#3498db',
+    '#9b59b6',
+    '#e74c3c',
+    '#2c3e50',
+    '#d35400',
+]
+
+let points = 0;
+let count = document.getElementById('score');
+let pointsElement = count.querySelector('.points');
+
+document.getElementById('btn').addEventListener('click', function(){
+    for (let i = 0; i < colors.length; i++) {
+        let box = document.createElement('div');
+        box.className = 'box';  
+
+        let fiveColor = colors[i];
+        box.style.backgroundColor = fiveColor;
+
+        box.addEventListener('click', function(){
+            box.parentNode.removeChild(box);
+            points--;
+            pointsUpdates();
+        });
+
+        document.querySelector(".boxes").appendChild(box);
+        points++;
+    }
+    pointsUpdates();
+})
+
+function pointsUpdates(){
+    pointsElement.innerHTML = points;
+}
